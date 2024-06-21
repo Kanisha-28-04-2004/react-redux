c2360caef539026d39a54b445c989977ad3a755d
Karthick1242004 <karthick1242004@gmail.com>
2024-06-21 08:27:54 +0530

    2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import '../App.css'
+import '../App.css';
 
 function Navbar({ isStudent, onToggle }) {
   return (
     <nav className="navbar">
       <h1>KEC</h1>
-      <button className='btn' onClick={onToggle}>
-        {isStudent ? 'Staff' : 'Student'}
-      </button>
+      <div className="toggle-container">
+        <span className="toggle-text"> Staff </span>
+        <div
+          className={`toggle-button ${isStudent ? 'active' : ''}`}
+          onClick={onToggle}
+        >
+          <div className="toggle-circle"></div>
+        </div>
+        <span className="toggle-text"> Student </span>
+      </div>
     </nav>
   );
 }
